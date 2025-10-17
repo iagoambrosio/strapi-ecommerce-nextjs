873dd6a6b86affded41bed3b8d1abdf90395df7d
Iago Silva Ambrosio <82549725+iagoambrosio@users.noreply.github.com>
2025-10-16 21:12:23 -0300

    ok

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: process.env.NEXT_PUBLIC_IMAGE_HOST_PROTOCOL,
-        hostname: process.env.NEXT_PUBLIC_IMAGE_HOST_NAME,
-        port: '',
-        pathname: "/**",
-      },
-    ],
-  },
-};
-
-module.exports = nextConfig;
+//const nextConfig = {
+//  reactStrictMode: false,
+//  images: {
+//    remotePatterns: [
+//      {
+//        protocol: process.env.NEXT_PUBLIC_IMAGE_HOST_PROTOCOL,
+//        hostname: process.env.NEXT_PUBLIC_IMAGE_HOST_NAME,
+//        port: '',
+//        pathname: "/**",
+//      },
+//    ],
+//  },
+//};
+//
+//module.exports = nextConfig;
