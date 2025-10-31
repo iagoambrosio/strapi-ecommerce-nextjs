2b3e53c5c79107660dcea1f211bf4b6c27714b18
Iago Silva Ambrosio <82549725+iagoambrosio@users.noreply.github.com>
2025-10-31 13:20:57 -0300

    new url

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: 'expert-waddle-qxjvxp4p56phgwr-1337.app.github.dev',
         pathname: "/**",
       },
+      {
+        protocol: 'https',
+        hostname: 'proxy-nginx-1.duckdns.org',
+        pathname: '/strapi-cms',
+      },
     ],
   },
 };
